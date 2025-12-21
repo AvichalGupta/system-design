b4e1eac081c24f754bb3fddcdc04fc9e176d34ed
Avichal Gupta <avichalgupta1999@gmail.com>
2025-12-21 23:52:03 +0530

    lru cache init

--- src/lru-cache/index.ts
+++ src/lru-cache/index.ts
@@ -1,19 +1,40 @@
+import { DoublyLinkedList, DoublyLinkedListNode, MAX_SIZE } from "@avichal-gupta/dsa";
+
 export class LRUCache<T> {
-    private cache: Map<string, T>;
+    private nodeMapper: Map<string, DoublyLinkedListNode<T>>;
+    private doublyLinkedList: DoublyLinkedList<T>;
+
     private maxSize: number;
-    constructor(maxSize: number, items: T[]) {
+    constructor(maxSize: number = MAX_SIZE) {
         if (maxSize < 0) throw new Error('Max size cannot be negative')
 
-        this.cache = new Map<string, T>();
+        this.nodeMapper = new Map<string, DoublyLinkedListNode<T>>();
+        this.doublyLinkedList = new DoublyLinkedList<T>({ maxSize });
         this.maxSize = maxSize || Number.MAX_SAFE_INTEGER;
-
     }
 
-    public add(key: string, value: T): void {
-        if (this.cache.size >= this.maxSize) {
-            
-        } else {
-            this.cache.set(key, value);
+    public get(key: string): T | null {
+        if (!this.nodeMapper.has(key)) {
+            return null;
         }
+        
+        const existingNode = this.nodeMapper.get(key);
+        this.doublyLinkedList.unlinkNode(existingNode!);
+        this.nodeMapper.delete(key);
+        const newNode: DoublyLinkedListNode<T> = this.doublyLinkedList.pushToFront(existingNode?.data!);
+        this.nodeMapper.set(key, newNode);
+        return newNode.data;
+    }
+
+    public put(key: string, value: T): void {
+        if (this.nodeMapper.has(key)) {
+            const existingNode = this.nodeMapper.get(key);
+            this.doublyLinkedList.unlinkNode(existingNode!);
+        } else if (this.nodeMapper.size >= this.maxSize) {
+            this.doublyLinkedList.popFromBack();
+        }
+        this.nodeMapper.delete(key);
+        const newNode: DoublyLinkedListNode<T> = this.doublyLinkedList.pushToFront(value);
+        this.nodeMapper.set(key, newNode);
     }
 }
