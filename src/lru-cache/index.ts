909fb19f77e461065aa0ad75fd440085e2a00aaf
Avichal Gupta <avichalgupta1999@gmail.com>
2025-12-22 01:16:38 +0530

    updating LRU implementation to handle capacity exceeded edge case

--- src/lru-cache/index.ts
+++ src/lru-cache/index.ts
@@ -1,15 +1,19 @@
 import { DoublyLinkedList, DoublyLinkedListNode, MAX_SIZE } from "@avichal-gupta/dsa";
 
+interface LRUInternalType<T> {
+    key: string
+    value: T
+}
 export class LRUCache<T> {
-    private nodeMapper: Map<string, DoublyLinkedListNode<T>>;
-    private doublyLinkedList: DoublyLinkedList<T>;
+    private nodeMapper: Map<string, DoublyLinkedListNode<LRUInternalType<T>>>;
+    private doublyLinkedList: DoublyLinkedList<LRUInternalType<T>>;
 
     private maxSize: number;
     constructor(maxSize: number = MAX_SIZE) {
         if (maxSize < 0) throw new Error('Max size cannot be negative')
 
-        this.nodeMapper = new Map<string, DoublyLinkedListNode<T>>();
-        this.doublyLinkedList = new DoublyLinkedList<T>({ maxSize });
+        this.nodeMapper = new Map<string, DoublyLinkedListNode<LRUInternalType<T>>>();
+        this.doublyLinkedList = new DoublyLinkedList<LRUInternalType<T>>({ maxSize });
         this.maxSize = maxSize || Number.MAX_SAFE_INTEGER;
     }
 
@@ -21,9 +25,9 @@ export class LRUCache<T> {
         const existingNode = this.nodeMapper.get(key);
         this.doublyLinkedList.unlinkNode(existingNode!);
         this.nodeMapper.delete(key);
-        const newNode: DoublyLinkedListNode<T> = this.doublyLinkedList.pushToFront(existingNode?.data!);
+        const newNode: DoublyLinkedListNode<LRUInternalType<T>> = this.doublyLinkedList.pushToFront(existingNode?.data!);
         this.nodeMapper.set(key, newNode);
-        return newNode.data;
+        return newNode.data?.value ?? null;
     }
 
     public put(key: string, value: T): void {
@@ -31,10 +35,17 @@ export class LRUCache<T> {
             const existingNode = this.nodeMapper.get(key);
             this.doublyLinkedList.unlinkNode(existingNode!);
         } else if (this.nodeMapper.size >= this.maxSize) {
-            this.doublyLinkedList.popFromBack();
+            const poppedNode = this.doublyLinkedList.popFromBack();
+            if (poppedNode.data) {
+                this.nodeMapper.delete(poppedNode.data.key);
+            }
         }
         this.nodeMapper.delete(key);
-        const newNode: DoublyLinkedListNode<T> = this.doublyLinkedList.pushToFront(value);
+        const lruInternalItf: LRUInternalType<T> = {
+            key,
+            value
+        }
+        const newNode: DoublyLinkedListNode<LRUInternalType<T>> = this.doublyLinkedList.pushToFront(lruInternalItf);
         this.nodeMapper.set(key, newNode);
     }
 
